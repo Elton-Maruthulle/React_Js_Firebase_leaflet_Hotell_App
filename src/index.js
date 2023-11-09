c84952d00c084c79c0f12dc8b8ab88dbd2e7796b
Elton-Maruthulle <eltonmaruthulle@gmail.com>
2023-11-09 20:41:02 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
 import Protected from './components/Protected';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
@@ -16,16 +9,14 @@ import BookingPage from './pages/Booking/BookingDate';
 import BookingRoom from './pages/Booking/Booking';
 import About from './pages/Hotel/components/About/About';
 import Contact from './pages/Hotel/components/Contact/Contact';
-import MapLocation from './pages/Hotel/components/Map/Location'
-
-
+import MapLocation from './pages/Hotel/components/Map/Location';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="signup" element={<Signup />} />
       <Route path="login" element={<Login />} />
-      <Route path="/" element={<Protected />} >
+      <Route path="/" element={<Protected />}>
         <Route path="/" index element={<Home />} />
         <Route path="booking" index element={<BookingPage />} />
         <Route path="bookingroom" index element={<BookingRoom />} />
@@ -34,10 +25,9 @@ const router = createBrowserRouter(
         <Route path="Mappage" index element={<MapLocation />} />
       </Route>
     </Route>
-  )
+  ),
+  { basename: '/React_Js_Firebase_leaflet_Hotel_App' }
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <RouterProvider router={router} />
-);
+const root = createRoot(document.getElementById('root'));
+root.render(<RouterProvider router={router} />);
